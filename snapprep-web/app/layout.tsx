a39e4fda325ae1ac75df02e2e520ede9581152eb
xx7yy7zz <diegokarim127@gmail.com>
2026-06-13 23:47:09 -0600

    chore: fix web title

--- snapprep-web/app/layout.tsx
+++ snapprep-web/app/layout.tsx
@@ -5,21 +5,20 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SnapPrep SAT",
-  description: "Your instant AI math tutor",
+    title: "EstudIAmigo",
+    description: "Tu tutor personal",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    // Add className="dark" right here to force the global dark mode theme
-    <html lang="en" className="dark">
-      <body className={`${inter.className} antialiased`}>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        // Add className="dark" right here to force the global dark mode theme
+        <html lang="en" className="dark">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
+
